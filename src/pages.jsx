3b0257672dfe396a4c285e499cf69f1b968d8ea4
prasanaseyon <75750776+prasanaseyon@users.noreply.github.com>
2026-08-10 12:42:23 +0530

    Update pages.jsx

--- src/pages.jsx
+++ src/pages.jsx
@@ -79,7 +79,7 @@ export function Home() {
 export function About() {
   return (
     <Page>
-      <PageHead eyebrow="About" title="" />
+      <PageHead eyebrow="" title="About" />
 
       <div className="about-grid">
         <div className="about-copy">
